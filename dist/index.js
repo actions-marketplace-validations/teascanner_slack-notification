fb60bc6823dfd97d14120719fb519b498ef53921
Matteo Pilone <pilone.matteo@gmail.com>
2021-09-27 01:12:51 +0200

    add new message on action

--- dist/index.js
+++ dist/index.js
@@ -131,20 +131,18 @@ __nccwpck_require__.r(__webpack_exports__);
 /* harmony export */ });
 const DEPLOY_INIT = (payload) => {
     return {
-        "blocks": [
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": `*${payload.repository.full_name} is deploying...*`
-                },
-                "accessory": {
-                    "type": "image",
-                    "image_url": "https://media4.giphy.com/media/xTkcEQACH24SMPxIQg/giphy.gif?cid=ecf05e47kpv37pdsny9ruerjn0p4t1u0brd9o3cuqit4jswx&rid=giphy.gif&ct=g",
-                    "alt_text": "cute cat"
-                }
-            }
-        ]
+
+        "type": "section",
+        "text": {
+            "type": "mrkdwn",
+            "text": `*${payload.repository.full_name} is deploying...*`
+        },
+        "accessory": {
+            "type": "image",
+            "image_url": "https://media4.giphy.com/media/xTkcEQACH24SMPxIQg/giphy.gif?cid=ecf05e47kpv37pdsny9ruerjn0p4t1u0brd9o3cuqit4jswx&rid=giphy.gif&ct=g",
+            "alt_text": "cute cat"
+        }
+
     }
 }
 
--- message/deploy_init.js
+++ message/deploy_init.js
@@ -1,18 +1,16 @@
 export const DEPLOY_INIT = (payload) => {
     return {
-        "blocks": [
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": `*${payload.repository.full_name} is deploying...*`
-                },
-                "accessory": {
-                    "type": "image",
-                    "image_url": "https://media4.giphy.com/media/xTkcEQACH24SMPxIQg/giphy.gif?cid=ecf05e47kpv37pdsny9ruerjn0p4t1u0brd9o3cuqit4jswx&rid=giphy.gif&ct=g",
-                    "alt_text": "cute cat"
-                }
-            }
-        ]
+
+        "type": "section",
+        "text": {
+            "type": "mrkdwn",
+            "text": `*${payload.repository.full_name} is deploying...*`
+        },
+        "accessory": {
+            "type": "image",
+            "image_url": "https://media4.giphy.com/media/xTkcEQACH24SMPxIQg/giphy.gif?cid=ecf05e47kpv37pdsny9ruerjn0p4t1u0brd9o3cuqit4jswx&rid=giphy.gif&ct=g",
+            "alt_text": "cute cat"
+        }
+
     }
 }
